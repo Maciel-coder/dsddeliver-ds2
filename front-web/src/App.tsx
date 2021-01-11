1c420c1b027973dfaf1a6bb36c077a913059576c
Maciel-coder <macielrodrigues940@gmail.com>
2021-01-10 23:00:50 -0300

    AddHome page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div >
+    <>
      <Navbar />
-    </div>
+     <Home />
+     
+    </>
   );
 }
 
